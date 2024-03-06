0499be34ec69e304e31659681d55e53d40bfa727
Pushcod <4programart1@gmail.com>
2024-03-06 19:27:42 +0300

    Создали базу данных для ресторана

---
+++ database/migrations/2024_03_06_161058_create_workers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('passport');
+            $table->string('number');
+            $table->string('mail');
+            $table->string('home');
+            $table->string('exp');
+            $table->boolean('workshift');
+            $table->time('worktime');
+            $table->string('jobtitle');
+            $table->string('salary');
+            $table->boolean('isActive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workers');
+    }
+};
